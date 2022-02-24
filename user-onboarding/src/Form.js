defe67c53caa963224ad3356090a77a7c1310fc5
eli-kirschbaum <eli.kirschbaum1@gmail.com>
2022-02-24 02:59:16 -0600

    added html markup in form and change handlers

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+
+
+export default function Form(props) {
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = evt => {
+        const { name, value, type, checked } = evt.target
+        const valueToUse = evt.target.type === 'checkbox' ? checked : value;
+        change(name, value)
+    }
+
+
+    return (
+        <div className='form-container'>
+            <div className='form-inputs'>
+                <label>Name
+                    <input
+                        value={values.name}
+                        onChange={onChange}
+                        name='name'
+                        type='text'
+                    />
+                </label>
+                <label>Email
+                    <input
+                        value={values.email}
+                        onChange={onChange}
+                        name='email'
+                        type='text'
+                    />
+                </label>
+                <label>Password
+                    <input
+                        value={values.password}
+                        onChange={onChange}
+                        name='password'
+                        type='text'
+                    />
+                </label>
+                <label>Terms of Service
+                    <input
+                        type='checkbox'
+                        name='terms-of-service'
+                        checked={values.tos}
+                        onChange={onChange}
+                    />
+                </label>
+                <button disabled={disabled}>Submit</button>
+            </div>
+        </div>
+    )
+}
